820f41639d24ff8c91c3118bd9d2847e59d29f7f
hdennen <hdennen@users.noreply.github.com>
2016-03-10 16:02:46 +0200

    Two Strings v2
    it works, but only with smaller inputs. Larger inputs timeout. Method
    still inefficient.

--- twoStrings.js
+++ twoStrings.js
@@ -6,6 +6,7 @@ function processData(input) {
     for(let i=0;i<arr.length;i+=2){ //loop every other item.
         let a = arr[i].split(''); //grab current item.
         let b = arr[i+1].split(''); //grab subsequent item.
+        let x = 0; //initialize loop counter because indexOf doesn't work when chars are same.
         mainloop: for(let valueA of a){ //loop through each letter of item a.
             for(let valueB of b){ //loop through each letter of item b while holding item a.
                 if(valueA === valueB){ //compare item a with item b.
@@ -13,7 +14,8 @@ function processData(input) {
                     break mainloop; //and break the loop labelled 'mainloop' because we can stop here.
                 }
             }
-            if(a.length-1 == a.indexOf(valueA)){ //if we've made it to the end without a match print no.
+            x++; //increment loop counter.
+            if(a.length-1 == x){ //if we've made it to the end without a match print no.
                 console.log('NO');
             }
         }
