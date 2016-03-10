24ca3065a575ddc0a463888cd6720669e3646f39
hdennen <hdennen@users.noreply.github.com>
2016-03-10 16:27:39 +0200

    Two Strings v3
    Still inefficient but pulled arr.length operations out of loops as they
    are O(n).

--- twoStrings.js
+++ twoStrings.js
@@ -2,11 +2,12 @@ function processData(input) {
     'use strict';
     const arr = input.split('\n'); //array of input lines.
     const t = parseInt(arr.shift()); //first line which also removes it from arr.
-    
-    for(let i=0;i<arr.length;i+=2){ //loop every other item.
+    const len = arr.length; //move O(n) operation out of for loop. 
+    for(let i=0;i<len;i+=2){ //loop every other item.
         let a = arr[i].split(''); //grab current item.
         let b = arr[i+1].split(''); //grab subsequent item.
         let x = 0; //initialize loop counter because indexOf doesn't work when chars are same.
+        let aLen = a.length; //again, move O(n) operation out.
         mainloop: for(let valueA of a){ //loop through each letter of item a.
             for(let valueB of b){ //loop through each letter of item b while holding item a.
                 if(valueA === valueB){ //compare item a with item b.
@@ -15,7 +16,7 @@ function processData(input) {
                 }
             }
             x++; //increment loop counter.
-            if(a.length-1 == x){ //if we've made it to the end without a match print no.
+            if(aLen-1 == x){ //if we've made it to the end without a match print no.
                 console.log('NO');
             }
         }
