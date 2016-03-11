f5018e17ab09ba2f97e3de39072d9425e01024e3
hdennen <hdennen@users.noreply.github.com>
2016-03-11 20:19:16 +0200

    Gem Stones v1
    This actually works. Dumb mistake in the first one.

--- gemStones.js
+++ gemStones.js
@@ -1,3 +1,4 @@
+//so dumb. I originally wrote the letter indexes as keys in the rocksAlph object, instead of writing the letter of the alphabet I was counting! And forgot to actually check that the letter existed in the rock string.
 function processData(input) {
     'use strict';
     const rocks = input.split('\n'); //grab each line, after shift creates array of rocks.
@@ -8,18 +9,16 @@ function processData(input) {
     for(let each of rocks){ //loop through each rock.
         for(let letter of alph){ //loop through the alphabet for each letter of each rock.
             let iAlph = each.indexOf(letter); //find the alphabet index of each letter in each rock.
-            if (!rocksAlph.hasOwnProperty(iAlph)){ //check if this letter index exists yet.
-                rocksAlph[iAlph] = 0; //if it doesn't, initialize our holder value,
-                rocksAlph[iAlph]++; //and increment it.
-            }else{
-                rocksAlph[iAlph]++; //otherwise just increment what already exists.
+            if (!rocksAlph.hasOwnProperty(letter) && iAlph > -1){ //check if this letter exists in the index, AND make sure the letter exists in the rock!!!
+                rocksAlph[letter] = 0; //if it doesn't, initialize our holder value,
+                rocksAlph[letter]++; //and increment it.
+            }else if(iAlph > -1){ //again, actually make sure the letter exists.
+                rocksAlph[letter]++; //and increment it.
             }
         }
     }
     for(let key in rocksAlph){ //loop by index for the object.
-        
-        //console.log(key+' '+rocksAlph[key]+' '+rocksAlph[-1]);
-        if(rocksAlph[key] == nR && key != '-1'){ //check that the number of letters equals the number of rocks and make sure it's not a false positive from happening to have the same number of non-index letters.
+        if(rocksAlph[key] == nR){ //check that the number of letters equals the number of rocks and make sure it's not a false positive from happening to have the same number of non-index letters.
             gemElements++; //if satisfied make it a gem element.
         }
     }
