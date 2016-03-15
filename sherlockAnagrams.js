3bb5deed9268aada3ccaab0ef29e677ca2445594
hdennen <hdennen@users.noreply.github.com>
2016-03-15 13:47:55 +0200

    Sherlock and Anagrams v0.1
    Still not there, but we now have accurate comparable substrings output.

--- sherlockAnagrams.js
+++ sherlockAnagrams.js
@@ -4,6 +4,7 @@ function processData(input) {
     'use strict';
     const strings = input.split('\n');
     const t = strings.shift();
+    let pairs = 0;
     for(let string of strings){ //operate on each string.
         let subs = [];
         let len = string.length;
@@ -14,6 +15,21 @@ function processData(input) {
             }
             subIn++;//this right here is the key to all this.
         }
-        console.log(subs);
+        //console.log(subs);
+        let sLen = subs.length; //length of substrings array.
+        let subStrBIn = 1; //substring B start index
+        for(let key in subs){ //first sub string loop
+            let subStrA = subs[key];
+            let aLen = subStrA.length; //good to pull these out so they don't get unnecessarily recalculated.
+            for(let i = subStrBIn; i<sLen; i++){ //comparitive substring loop
+                let subStrB = subs[i];
+                let bLen = subStrB.length;
+                if (aLen === bLen){
+                    console.log(subStrA+' '+subStrB);
+                } 
+            }
+            subStrBIn++;
+        }
+        //console.log(pairs);
     }
 } 
