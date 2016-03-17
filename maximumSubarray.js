b40b78a811a27f5e9aeeeed4491e25d68833ebb9
hdennen <hdennen@users.noreply.github.com>
2016-03-17 10:30:18 +0200

    The Maximum Subarray v0.1
    calculates contiguous maximum subarray.

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -1,35 +1,38 @@
 //https://www.hackerrank.com/challenges/maxsubarray
-//this doesn't work yet.
+//only calculates contiguous max.
 function processData(input) {
     'use strict';
     const tests = input.split('\n');
     const t = tests.shift();
     const range = function(array, end){ //helper function to create a subarray by range
         let a = [];
-        let i = 0;
-        while(i<=end){
-            a.push(array[i])
-            i++;
+        let ai = 0;
+        while(ai<=end){
+            a.push(array[ai]);
+            ai++;
         }
         return a;
     };
-    const findMax = function(test, currentMax){
+    const findMax = function(test, tempMax, currentMax){ //recursive function to find max of contiguous array.
         for(let i in test){
-            let tempMax = range(test,i).reduce((b,a)=> b+a);
+            tempMax = range(test,i).reduce((b,a)=> b+a);
             if(tempMax > currentMax){
                 currentMax = tempMax;
             }
         }
         test.shift();
         if(test.length > 0){
-            findMax(test);
+            findMax(test, tempMax, currentMax);
+        }else{
+            max = currentMax;
         }
     };
-    for (let i=1; i<t*2; i+=2){
+    for (let i=1; i<t*2; i+=2){ //loop through each test
         let test = tests[i].split(' ').map(Number);
         let currentMax = 0;
-        //console.log(test);
-        findMax(test);
-        console.log(currentMax);
+        let tempMax = 0;
+        var max = 0; 
+        findMax(test, tempMax, currentMax);
+        console.log(max);
     }
 } 
