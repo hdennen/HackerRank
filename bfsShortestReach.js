5261acc05e553140202dd849a3a1804e9c781362
hdennen <hdennen@users.noreply.github.com>
2016-03-22 15:02:50 +0200

    v0.5
    Seems like this should work.

--- bfsShortestReach.js
+++ bfsShortestReach.js
@@ -6,45 +6,89 @@ function processData(input) {
     let nTests = lines.shift();
     let i = 0; // should bring us to the beginning of each test.
     while(nTests>0){
-        let visited = []; //the visited node distance.
-        let queue = [];
-        let answer = '';
         let ne = lines[i].split(' '); //temp
         let numNodes = parseInt(ne.shift()); //number of nodes
         let numEdges = parseInt(ne); //number of edges
         let startPos = parseInt(lines[i+1+numEdges]); //starting position
-        let nodes = [];
-        let c = 1;
-        //create object of nodes and degrees of separation from start node.
+        let level = 0;
+        let distances = {}; //object to keep track what level each item in queue is on.
+        let visited = [startPos];
+        let queue = [];
         let edges =[];
-        for(let a = i+1; a<=i+numEdges; a++){ //fill array of edges.
-            edges.push(lines[a].split(' ').map(Number));
-        }
         
+        let nodesArr = [];
+        let c = 1;
         while(numNodes>0){ //create queue of nodes.
-            nodes.push(c)
+            nodesArr.push(c)
             c++;
             numNodes--;
         }
-        nodes.splice(nodes.indexOf(startPos), 1); //remove start node from nodes
+        nodesArr.splice(nodesArr.indexOf(startPos), 1); //remove start node from nodes
         
+        for(let a = i+1; a<=i+numEdges; a++){ //fill array of edges.
+            edges.push(lines[a].split(' ').map(Number));
+        }
         queue.push(startPos); //add start position to queue.
+        let counter = 0;
         while(queue.length > 0){ //use whatever is at start of queue
             let n = queue[0];
+             //counts queue additions to keep track of levels.
+            if(counter == 0){
+                level++;
+            }else{
+                counter--;
+            }
             for(let edge of edges){ //this could be optimized by sanitizing edges array of used edges.
-                if((edge.indexOf(n) == 1) && (nodes.indexOf(edge[0]) > -1)){
-                   visited.push(edge[0]);
-                   queue.push(edge[0]);
-                }else if((edge.indexOf(n) == 0) && (nodes.indexOf(edge[1]) > -1)){
-                   visited.push(edge[1]);
-                   queue.push(edge[1]);
-                } 
+                let ni = edge.indexOf(n);
+                let vie0 = visited.indexOf(edge[0]);
+                let vie1 = visited.indexOf(edge[1]);
+                
+                if(ni == 1 && vie0 == -1){ //if contains n and isn't already visited.
+                    visited.push(edge[0]);
+                    queue.push(edge[0]);
+                    
+                    if(!distances[level]){
+                        distances[level] = [];
+                        distances[level].push(edge[0]);
+                    }else{
+                        distances[level].push(edge[0]);
+                    }
+                    
+                    counter++;
+                    
+                }else if(ni == 0 && vie1 == -1){
+                    visited.push(edge[1]);
+                    queue.push(edge[1]);
+                    
+                    if(!distances[level]){
+                        distances[level] = [];
+                        distances[level].push(edge[1]);
+                    }else{
+                        distances[level].push(edge[1]);
+                    }
+                    counter++;
+                }
+                    
             }
             queue.shift();
         }
 
-        console.log(visited);
+        let answer = ''; //get the answer ready.
+        
+        for (let n of nodesArr){ //loop through nodes.
+            for(let l in distances){ //for each node, loop through distances object.
+                if(distances[l].indexOf(n) == -1){
+                    answer += '-1 ';
+                }
+                else{
+                    let trueDistance = l*6;
+                    answer += trueDistance+' ';
+                }
+            }
+        }
 
+        console.log(answer);
+        
         i += numEdges+2; //increment to beginning of next test
         nTests--; //decrement tests
     }
