5848156ec3d5b45fb20b4ea3137b1cbe0efc4a6d
hdennen <hdennen@users.noreply.github.com>
2016-03-22 10:32:36 +0200

    v0.3
    works for edges that begin at start position but doesn't find min
    distance properly.

--- bfsShortestReach.js
+++ bfsShortestReach.js
@@ -1,21 +1,48 @@
 //https://www.hackerrank.com/challenges/bfsshortreach
-//just get through the input...
+//works for edges that begin at start position but doesn't find min distance properly.
 function processData(input) {
     'use strict';
     const lines = input.split('\r\n'); // well carriage return is new. \r.
     let nTests = lines.shift();
     let i = 0; // should bring us to the beginning of each test.
     while(nTests>0){
+        let visited = [];
+        let answer = '';
         let ne = lines[i].split(' '); //temp
-        let nodes = parseInt(ne.shift()); //number of nodes
+        let numNodes = parseInt(ne.shift()); //number of nodes
         let numEdges = parseInt(ne); //number of edges
-        let startPos = lines[i+1+numEdges]; //starting position
-        
-        let edges =[];
-        for(let a = i+1; a<=numEdges; a++){ //loop through each edge of current test.
-            edges.push(lines[a]);
+        let startPos = parseInt(lines[i+1+numEdges]); //starting position
+        let nodes = []
+        let c = 1;
+        while(numNodes>0){ //create queue of nodes.
+            nodes.push(c)
+            c++;
+            numNodes--;
         }
-        console.log(edges);
+        nodes.splice(nodes.indexOf(startPos), 1);//remove start node from nodes
+
+        let edges =[];
+        for(let a = i+1; a<=i+numEdges; a++){ //fill array of edges.
+            edges.push(lines[a].split(' ').map(Number));
+        }
+        for(let edge of edges){
+            if((edge.indexOf(startPos) > -1) && (nodes.indexOf(edge[0]) > -1)){
+               visited.push(edge[0]);
+               
+            }else if((edge.indexOf(startPos) > -1) && (nodes.indexOf(edge[1]) > -1)){
+               visited.push(edge[1])
+               
+            } 
+        }
+        
+        for(let n of nodes){
+            if(visited.indexOf(n) > -1){
+                answer += '6 ';
+            }else{
+                answer += '-1 ';
+            }
+        }
+        console.log(answer);
         i += numEdges+2; //increment to beginning of next test
         nTests--; //decrement tests
     }
