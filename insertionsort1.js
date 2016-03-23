3da5e45f5c20aa2460ba232e9a0ac65e9a2af3bc
hdennen <hdennen@users.noreply.github.com>
2016-03-23 15:55:04 +0200

    v1
    have to break the loop once number is inserted. and account for e being
    first in array.

--- insertionsort1.js
+++ insertionsort1.js
@@ -5,12 +5,17 @@ function processData(input) {
     const arr = size.pop().split(' ');
     const e = arr.slice(-1).join('');
     for(let i = size-2; i>=0; i--){
-        
         if(arr[i] > e){
             arr.splice(i+1,1,arr[i]);
+            console.log(arr.join(' '));
         }else if(arr[i] <= e){
             arr.splice(i+1,1,e);
+            console.log(arr.join(' '));
+            break;
         }
-        console.log(arr.join(' '));
+        if(i == 0){ //take into account reaching the end of the array.
+            arr.splice(i,1,e);
+            console.log(arr.join(' '));
+        }
     }
 }
