e1271ee989df7a157178157015b96ec04efdea00
hdennen <hdennen@users.noreply.github.com>
2016-03-23 12:15:10 +0200

    v1.2
    removed visited array and all push() funtions.

--- bfsShortestReach.js
+++ bfsShortestReach.js
@@ -1,5 +1,5 @@
 //https://www.hackerrank.com/challenges/bfsshortreach
-//Using queue.js
+//removed visited array and all push() funtions.
 function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
 
 function processData(input) {
@@ -14,7 +14,6 @@ function processData(input) {
         let startPos = parseInt(lines[i+1+numEdges]); //starting position
         
         let distances = {}; //object to keep track what level each item in queue is on.
-        let visited = [startPos];
         let edges =[];
         
         for(let a = i+1; a<=i+numEdges; a++){ //fill array of edges.
@@ -29,14 +28,12 @@ function processData(input) {
             for(let edge of edges){ //this could be optimized by sanitizing edges array of used edges.
                 let ni = edge.indexOf(n);
                 if(ni == 1 || ni == 0){
-                    let vie0 = visited.indexOf(edge[0]);
-                    let vie1 = visited.indexOf(edge[1]);
-                    if(ni == 1 && vie0 == -1){ //if contains n and isn't already visited.
-                        visited.push(edge[0]); 
+                    let vie0 = distances[edge[0]] === undefined;
+                    let vie1 = distances[edge[1]] === undefined;
+                    if(ni == 1 && vie0 == true){ //if contains n and isn't already visited.
                         subQueue.enqueue(edge[0]);
                         distances[edge[0]] = level; //track which level we're on.
-                    }else if(ni == 0 && vie1 == -1){
-                        visited.push(edge[1]);
+                    }else if(ni == 0 && vie1 == true){
                         subQueue.enqueue(edge[1]);
                         distances[edge[1]] = level; //track which level we're on.
                     }
@@ -71,14 +68,3 @@ function processData(input) {
         nTests--; //decrement tests
     }
 } 
-
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
-
-process.stdin.on("end", function () {
-   processData(_input);
-});
