8830cf4c05fa7b3cd9b810817b0fdd195216a59a
hdennen <hdennen@users.noreply.github.com>
2016-03-23 13:46:48 +0200

    v1.3
    100% working. for of is not as fast as cached regular for loop.

--- bfsShortestReach.js
+++ bfsShortestReach.js
@@ -1,5 +1,5 @@
 //https://www.hackerrank.com/challenges/bfsshortreach
-//removed visited array and all push() funtions.
+//100% working. for of is not as fast as cached regular for loop.
 function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
 
 function processData(input) {
@@ -22,10 +22,12 @@ function processData(input) {
         let queue = new Queue();
         let level = 1;
         let subQueue = new Queue();
+        let len = edges.length;
         queue.enqueue(startPos); //add start position to queue.
         while(!queue.isEmpty()){ //use whatever is at start of queue
             let n = queue.dequeue(); //start at beginning of queue.
-            for(let edge of edges){ //this could be optimized by sanitizing edges array of used edges.
+            for(let i = 0; i < len; i++){ //trying cached for loop instead of for of.
+                let edge = edges[i];
                 let ni = edge.indexOf(n);
                 if(ni == 1 || ni == 0){
                     let vie0 = distances[edge[0]] === undefined;
