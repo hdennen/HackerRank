e3c977aa9e7f097993d2befd6bf631126b9cb63f
hdennen <hdennen@users.noreply.github.com>
2016-03-23 15:23:01 +0200

    v0.1
    Only works on sample.

---
+++ insertionsort1.js
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/insertionsort1
+function processData(input) {
+    'use strict';
+    const size = input.split('\n');
+    const arr = size.pop().split(' ');
+    const e = arr.slice(-1).join('');
+    for(let i = size-2; i>=0; i--){
+        
+        if(arr[i] > 3){
+            arr.splice(i+1,1,arr[i]);
+        }else{
+            arr.splice(i+1,1,e);
+        }
+        console.log(arr.join(' '));
+    }
+}
