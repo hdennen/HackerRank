0c04de65fef48950e54109d5634eb33021d6948a
hdennen <hdennen@users.noreply.github.com>
2016-03-23 17:29:07 +0200

    v0.3
    needed to add breaks to while loop after insertion so it wouldn't fire
    again on a duplicate. still wrong tho.

--- insertionsort2.js
+++ insertionsort2.js
@@ -1,5 +1,5 @@
 //https://www.hackerrank.com/challenges/insertionsort2
-//Doesn't work if a duplicate is at the beginning of array
+//needed to add breaks to while loop after insertion so it wouldn't fire again on a duplicate.
 function processData(input) {
     'use strict';
     const size = input.split('\n');
@@ -17,9 +17,11 @@ function processData(input) {
                 if(arr[ib]<e){
                     arr.splice(ib+1,0,e); //slices e into previous position
                     console.log(arr.join(' '));
+                    break; //have to break the loop in case there are duplicates.
                 }else if(ib == 0){
                     arr.splice(ib,0,e); //slices e into current pos if at index 0.
                     console.log(arr.join(' '));
+                    break;
                 }
             }
         }else{
