59d0e2cfd138e8d4c1b36a4715c916eab5086f03
hdennen <hdennen@users.noreply.github.com>
2016-04-01 14:17:41 +0200

    v0.2
    times out, the constant split and sum is > O(n)

--- sherlockArray.js
+++ sherlockArray.js
@@ -6,20 +6,27 @@ function processData(input) {
     const stop = t*2-1;
     for (let i=0; i<stop; i+=2){
         let len = lines[i];
-        let arr = lines[i+1].split(' ').map(Number);
-        let j = 0;
-        while(j<len){
-            j++;
-            if(j === len-1){
-                console.log('NO');
-                break;
-            }
-            let left = arr.slice(0,j).reduce((a,b)=>a+b);
-            let right = arr.slice(j+1,len).reduce((a,b)=>a+b);
-            if(left === right){
-                console.log('YES');
-                break;
+        if(len == 1){
+            console.log('YES');
+        }else if(len == 2){
+            console.log('NO');
+        }else if(len >2){
+            let arr = lines[i+1].split(' ').map(Number);
+            let j = 0;
+            while(j<len){
+                j++;
+                if(j == len-1){
+                    console.log('NO');
+                    break;
+                }
+                let left = arr.slice(0,j).reduce((a,b)=>a+b);
+                let right = arr.slice(j+1,len).reduce((a,b)=>a+b);
+                if(left === right){
+                    console.log('YES');
+                    break;
+                }
             }
         }
+
     }
 } 
