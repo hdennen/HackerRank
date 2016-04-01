faac0a82d342c1d9b295075b35d7ca8b6c0444b3
hdennen <hdennen@users.noreply.github.com>
2016-04-01 16:11:18 +0200

    v0.1
    crazy way to do this... doesn't work.

---
+++ iceCreamParlor.js
@@ -0,0 +1,43 @@
+//https://www.hackerrank.com/challenges/icecream-parlor
+function processData(input) {
+    'use strict';
+    const lines = input.split('\n');
+    const t = lines.shift();
+    const len = t*3;
+    
+    for(let i=0; i<len; i+=3){
+        let m = lines[i];
+        let n = lines[i+1];
+        let arr = lines[i+2].split(' ').map(Number);
+        let arr2 = lines[i+2].split(' ').map(Number).sort((a,b)=>a-b);
+        let j = 0
+        if (arr2.indexOf(m) != -1){
+            j = arr2.indexOf(m)-1; //use this for j
+        }else{
+            for(j=0;j<n;j++){ //or go find j
+                if(arr2[j]>m){
+                    j--;
+                    break;
+                }
+            }
+        }
+        let jNum = 0;
+        let pNum = 0;
+        big: while(j--){
+            let p = 0;
+            jNum = arr2[j];
+            let mark = m-jNum;
+            while(p<j){
+                pNum = arr2[p];
+                if(pNum == mark){
+                    break big;
+                }
+                p++;
+            }
+        }
+        let pI = arr.indexOf(pNum)+1;
+        arr.splice(pNum,1);
+        let jI = arr.indexOf(jNum)+2;
+        console.log(pI+" "+jI);
+    }
+} 
