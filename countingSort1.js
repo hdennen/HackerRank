2bbe74699c31c0bbad888eb090c225a17742c1dc
hdennen <hdennen@users.noreply.github.com>
2016-04-12 14:00:45 +0200

    v2 the efficient way to do a count sort.

--- countingSort1.js
+++ countingSort1.js
@@ -4,18 +4,9 @@ function processData(input) {
     let arr = input.split('\n');
     const len = arr.shift();
     arr = arr[0].split(' ').map(Number);
-    let occur = [];
-    let i=0;
-    let c=0;
-    while(i<100){
-        c = 0;
-        for(let n of arr){
-            if(n===i){
-                c++;
-            }
-        }
-        occur.push(c);
-        i++;
+    let occur = new Array(100).fill(0);
+    for(let i=0;i<len;i++){
+        occur[arr[i]]++;
     }
     console.log(occur.join(' '));
 } 
