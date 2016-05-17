bb112d8051e5dc6846de9233d999ef0c54bbb2c1
hdennen <hdennen@users.noreply.github.com>
2016-05-17 20:13:35 +0200

    success

--- fibonacciModified.py
+++ fibonacciModified.py
@@ -1,8 +1,6 @@
 //https://www.hackerrank.com/challenges/fibonacci-modified
 //python 2
 
-import math
-
 arr = map(long, raw_input().split())
 a = arr[0]
 b = arr[1]
@@ -10,7 +8,7 @@ n = arr[2]
 
 def fibMod(first, second, final):
     while(final>2):
-        third = math.pow(second, 2) + first
+        third = second*second + first
         first = second
         second = third
         final = final - 1
